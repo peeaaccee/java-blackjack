3bb63567e9c80b2508f9d4497c6b66b66fd848ce
JIHYEISWISDOM <wlgp8958@naver.com>
2022-12-12 00:57:55 +0900

    feat(RentCar): 렌트카 & 테스트

---
+++ src/main/java/rentcar/Car.java
@@ -0,0 +1,17 @@
+package rentcar;
+
+public abstract class Car {
+    private final int distance;
+
+    public Car(int distance) {
+        this.distance = distance;
+    }
+
+    abstract double getDistancePerLiter();
+
+    abstract String getName();
+
+    double getChargeQuantity() {
+        return distance / getDistancePerLiter();
+    }
+}
